3ff4a7b01b01ac701a74695e10405ec2ade820ce
unknown <abellamechaela@gmail.com>
2025-09-15 20:25:49 +0800

    Upload

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("\nEnter string: ");
+                string input = Console.ReadLine();
+
+                if (input == "*")
+                {
+                    Console.WriteLine("\n//End execution");
+                    break;
+                }
+
+                string original = input;
+                string lower = input.ToLower();
+
+                string reversed = "";
+                for (int i = lower.Length - 1; i >= 0; i--)
+                {
+                    reversed += lower[i];
+                }
+
+                
+                string result = (lower == reversed) ? "Yes" : "No";
+
+                Console.WriteLine("\nOriginal String\t\tReversed String\t\tPalindrome?");
+                Console.WriteLine($"  {original}\t\t\t{reversed}\t\t\t{result}");
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+
+}
---
+++ HW2/HW2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+   
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter row: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter col: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            double[,] numbers = new double[rows, cols];
+            double sum = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine($"Row {i + 1}");
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Enter number{j + 1}: ");
+                    numbers[i, j] = double.Parse(Console.ReadLine());
+                    sum += numbers[i, j];
+                }
+            }
+
+            
+            Console.WriteLine("\nThe numbers are:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(numbers[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+           
+            double average = sum / (rows * cols);
+
+            Console.WriteLine($"\nSum: {sum} ; Average: {average}");
+            Console.ReadKey();
+        }
+    }
+
+}
